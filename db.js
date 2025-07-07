31d48c50122bb940fcbeaa9e185f04435b502dde
sidicksino <sidickabdoulayesino1@gmail.com>
2025-07-07 17:29:54 +0200

    Initial backend setup with Express, MySQL and dotenv

---
+++ db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+const db = mysql.createConnection({
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.PASSWORD,
+    database:process.env.DB_NAME
+});
+
+
+db.connect((err)=>{
+    if(err) throw err;
+    console.log("Connected to database ")
+});
+
+
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+const db= require('./db')
+
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+app.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
+})
