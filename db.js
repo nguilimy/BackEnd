c2a4294a9e9475fc46aa43fdf313dd92fda12127
sidicksino <sidickabdoulayesino1@gmail.com>
2025-07-09 00:42:22 +0200

    Add Register and Login Routes with JWT Authentication

--- db.js
+++ db.js
@@ -17,3 +17,6 @@ db.connect((err)=>{
 });
 
 
+module.exports = db;
+
+
--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 const db= require('./db')
+const userRoutes = require('./routes/userRoutes')
 
 dotenv.config();
 const port = process.env.PORT || 5000;
+app.use(express.json());
+
+app.use('/api/user', userRoutes);
 
 app.listen(port, () =>{
     console.log(`Server is running on port ${port}`);
