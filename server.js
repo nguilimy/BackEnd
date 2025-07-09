4250668590967959ae237ca54924a3fc22dfe168
sidicksino <sidickabdoulayesino1@gmail.com>
2025-07-09 14:34:39 +0200

    Add Forgot Password Flow with OTP Verification (Request, Verify, Reset)

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
-const db= require('./db')
-const userRoutes = require('./routes/userRoutes')
-const meRoutes = require('./routes/profileRoutes')
+const db= require('./db');
+const userRoutes = require('./routes/userRoutes');
+const meRoutes = require('./routes/profileRoutes');
+const resetPasswordRoute = require('./routes/resetPassordRoute');
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -11,7 +12,8 @@ app.use(express.json());
 
 app.use('/api/user', userRoutes);
 app.use('/api/me', meRoutes);
+app.use('/api/resetPassword', resetPasswordRoute);
 
 app.listen(port, () =>{
     console.log(`Server is running on port ${port}`);
-})
+});
